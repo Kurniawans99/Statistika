55c1bd06a69ece61a4e5a5329ce34f9ac47c32be
Kurniawans99 <egikurniawan2490@gmail.com>
2024-05-20 23:08:01 +0700

    fixed name input

--- home.py
+++ home.py
@@ -6,15 +6,15 @@ data = []
 st.title('Statistika')
 
 with st.form(key='data_form'):
-    intRate = st.number_input("Suku Bunga", min_value=0.0, max_value=1.0,value= 0.0 , step = .1,)
-    instalment = st.number_input("Cicilan", min_value=100, max_value=100000,value= 100 , step = 50,)
+    intRate = st.number_input("Tingkat Suku Bunga (0-1) ", min_value=0.0, max_value=1.0,value= 0.0 , step = .1,)
+    instalment = st.number_input(" Angsuran bulanan yang harus dibayarkan (cicilan) ", min_value=100, max_value=100000,value= 100 , step = 50,)
     dti = st.number_input("Rasio Pendapatan", min_value=0.0, max_value=1.0,value= 0.0 , step = .1,)
-    fico = st.number_input("Skor Kredit", min_value=100, max_value=1000,value= 100 , step = 10,)
-    daysWithCrLine = st.number_input("Tenggat Hutang", min_value=120, max_value=1826,value= 120 , step = 30,)
-    revolBal = st.number_input("Tunggakan", min_value=100, max_value=100000,value= 100 , step = 50,)
+    fico = st.number_input("Skor Kredit (300-850)", min_value=300, max_value=850,value= 350 , step = 10,)
+    daysWithCrLine = st.number_input("hari pertama peminjam memiliki akun kredit aktif (hari)", min_value=1, max_value=100000,value= 120 , step = 30,)
+    revolBal = st.number_input("jumlah hutang yang belum dibayar pada akhir siklus penagihan kartu kredit.", min_value=100, max_value=100000,value= 100 , step = 50,)
     revolUtil = st.number_input("Penggunaan Kredit", min_value=1, max_value=100,value= 1 , step = 1,)
-    delinq2yrs = st.number_input("Keterlambatan 2 Tahun", min_value=0, max_value=1,value= 0 , step = 1,)
-    inqLast6mths = st.number_input("Pertanyaan 6 Bulan Terakhir", min_value=0, max_value=1,value= 0 , step = 1,)
+    delinq2yrs = st.number_input("Jumlah kali peminjam terlambat membayar lebih dari 30 hari dalam 2 tahun terakhir.", min_value=0, max_value=1000,value= 0 , step = 1,)
+    inqLast6mths = st.number_input("Jumlah penyelidikan kredit oleh kreditur dalam 6 bulan terakhir.", min_value=0, max_value=10000,value= 0 , step = 1,)
 
     submit_button = st.form_submit_button(label='Submit')
 
